9d57fb3bd21c3a655f195b6536edcfe24be0619e
root <root@DESKTOP-CHQFLH6.localdomain>
2021-10-14 16:24:39 +0100

    Fix bug where add_semitones function would split chords and only add the notes to the increased_notes array. Now it adds a new increased chord

--- main.py
+++ main.py
@@ -3,12 +3,13 @@
 from parse_midi import *
 
 #Parse notes in one midi file
-#notes = parse_midi("data/chopin_winter_wind.mid")
-notes = parse_midi("data/for_elise_by_beethoven.mid")
+notes = parse_midi("data/chopin_winter_wind.mid")
+#notes = parse_midi("data/for_elise_by_beethoven.mid")
 #print("Result: \n", result)
 
 # Incease notes by a tone and add them to array
 increased_notes = add_semitones(notes, 2)
 #print("Increased notes: \n", increased_notes)
 
-output_midi(increased_notes, "fur_elise")
+#output_midi(increased_notes, "fur_elise")
+output_midi(increased_notes, "chopin_winter")
--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 #File for testing various things outside of maine
 from parse_midi import *
+from music21 import *
 
-result = parse_midi("for_elise_by_beethoven.mid")
-print("Result: \n", result)
+dMaj = chord.Chord(['D', 'F#', 'A'])
+myChord = chord.Chord('A4 C#5 E5')
+
+notes = []
+notes.append('.'.join(str(n) for n in dMaj.normalOrder))
+notes.append('.'.join(str(n) for n in myChord.normalOrder))
+
+print("Notes: ", notes)
+
+output_midi(notes, "chord_test")
+increased_chords = add_semitones(notes, 4)
+output_midi(increased_chords, "increased_chords")
